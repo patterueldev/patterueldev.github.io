f0b8678e9de48de1349fb5d0fb042b591cb66515
John Patrick Teruel <jpteruel95@gmail.com>
2023-03-12 19:08:21 +0800

    Deployed from patteruel-io repository

---
+++ js/about.8cedfdf2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpatteruel_io"]=self["webpackChunkpatteruel_io"]||[]).push([[443],{6506:function(t,e,n){n.r(e),n.d(e,{default:function(){return f}});var u=n(3396);const a={class:"about"},r=(0,u._)("h1",null,"This is an about page",-1),s=[r];function i(t,e){return(0,u.wg)(),(0,u.iD)("div",a,s)}var c=n(89);const o={},l=(0,c.Z)(o,[["render",i]]);var f=l}}]);
+//# sourceMappingURL=about.8cedfdf2.js.map
